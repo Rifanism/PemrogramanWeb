81ccb927087eda809b5f42fbe32977833385c577
Rifanism <rifanhabibi212205@gmail.com>
2026-02-18 21:49:15 +0700

    comm

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Assalamualaikum</h1>
+    <p>
+        <?php
+        echo "Rifann";
+        echo "<br>Test";
+        ?>
+    </p>
+</body>
+</html>
